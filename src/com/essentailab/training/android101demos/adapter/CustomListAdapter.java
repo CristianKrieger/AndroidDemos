b67903aaa04b25f3c6067632894cb19faf28c813
cristiankrieger <crstn.krieger@gmail.com>
2013-07-18 11:27:03 -0500

    Project Addition

---
+++ src/com/essentailab/training/android101demos/adapter/CustomListAdapter.java
@@ -0,0 +1,65 @@
+package com.essentailab.training.android101demos.adapter;
+
+import java.util.ArrayList;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.essentailab.training.android101demos.entities.ListElementCookie;
+
+public class CustomListAdapter extends BaseAdapter{
+	private ArrayList<ListElementCookie> data;
+	private int layoutId;
+	private int textItemId;
+	private int imageId;
+	private LayoutInflater inflater;
+	private int textDesId;
+	
+	public CustomListAdapter(LayoutInflater inflater,
+			ArrayList<ListElementCookie> data, int layoutId,
+			int textItemId, int textDesId, int imageId) {
+		this.inflater=inflater;
+		this.data = data;
+		this.layoutId = layoutId;
+		this.textDesId=textDesId;
+		this.textItemId = textItemId;
+		this.imageId=imageId;
+	}
+
+	@Override
+	public int getCount() {
+		return data.size();
+	}
+
+	@Override
+	public ListElementCookie getItem(int position) {
+		return data.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if(convertView==null)
+			convertView = inflater.inflate(layoutId, null);
+		ListElementCookie current = getItem(position);
+		
+		TextView tvT = (TextView) convertView.findViewById(textItemId);
+		tvT.setText(current.getTitle());
+		
+		TextView tvD = (TextView) convertView.findViewById(textDesId);
+		tvD.setText(current.getDescription());
+		
+		ImageView iv = (ImageView) convertView.findViewById(imageId);
+		iv.setImageDrawable(current.getThumb());
+		
+		return convertView;
+	}
+}
---
+++ src/com/essentailab/training/android101demos/fragment/CustomViewExampleFragment.java
@@ -0,0 +1,40 @@
+package com.essentailab.training.android101demos.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.essentailab.training.android101demos.R;
+import com.essentailab.training.android101demos.view.CardView;
+
+public class CustomViewExampleFragment extends SherlockFragment{
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_customview, container, false);
+		CardView c1 = (CardView) v.findViewById(R.id.frag_custom_card1);
+		CardView c2 = (CardView) v.findViewById(R.id.frag_custom_card2);
+		CardView c3 = (CardView) v.findViewById(R.id.frag_custom_card3);
+		
+		String defaultString =
+				"Lorem ipsum dolor sit amet," +
+				"consectetur adipisicing elit," +
+				"sed do eiusmod tempor incididunt" +
+				"ut labore et dolore magna aliqua.";
+		
+		c1.setText("Card 1: Blue");
+		c1.setBackText(defaultString);
+		c1.setOnClickListener(c1.listener);
+		
+		c2.setText("Card 2: Red");
+		c2.setBackText(defaultString);
+		c2.setOnClickListener(c2.listener);
+		
+		c3.setText("Card 3: Green");
+		c3.setBackText(defaultString);
+		c3.setOnClickListener(c3.listener);
+		return v;
+	}
+}
